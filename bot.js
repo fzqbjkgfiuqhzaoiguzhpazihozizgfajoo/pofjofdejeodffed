853bf9db437c97f19e3c8c8f50eff1eb825cda7d
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-15 18:40:03 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('-wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("dnd")
+    message.channel.send(` ** Done. :white_check_mark: ** `)
+} else 
+if (message.content.startsWith('-ls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** Done. :white_check_mark: ** `)
+} else 
+if (message.content.startsWith('-st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/ii7m0fy");
+  client.user.setStatus("dnd")
+   message.channel.send(` ** Done. :white_check_mark: ** `)
+} else 
+if (message.content.startsWith('-pl')) {
+client.user.setActivity(argresult , {type:'PLAYING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** Done. :white_check_mark: ** `)
+
+}
+client.login(process.env.BOT_TOKEN);
