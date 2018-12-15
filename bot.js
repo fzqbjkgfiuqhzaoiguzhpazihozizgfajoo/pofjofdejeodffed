e59c07a0889c7666a9e02d4079f454efa2f42c97
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-15 18:52:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const developers = ['343743154429755392','520562326676570112'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('-wt')) {
+if (message.content.startsWith('Hwt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
   client.user.setStatus("dnd")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
-if (message.content.startsWith('-ls')) {
+if (message.content.startsWith('Zls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
   client.user.setStatus("idle")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
-if (message.content.startsWith('-st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/ii7m0fy");
+if (message.content.startsWith('Zst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/ii7m0dy");
   client.user.setStatus("dnd")
    message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
-if (message.content.startsWith('-pl')) {
+if (message.content.startsWith('Zpl')) {
 client.user.setActivity(argresult , {type:'PLAYING'});
   client.user.setStatus("idle")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 
 }
+  client.on('message', message => {//iiHouSSaM#9494
+        if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
+  if (message.content === '963') {//iiHouSSaM#9494
+
+  let channel = client.channels.get('520562326676570112');//iiHouSSaM#9494
+
+
+  channel.join()//iiHouSSaM#9494
+  .then(connection => console.log(`????`))//iiHouSSaM#9494
+  .catch(console.error);//iiHouSSaM#9494
+  }
+});
+          
+});
 client.login(process.env.BOT_TOKEN);
